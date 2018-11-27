14ae6f89be57c86b0a92303148c51e5d78571266
Luiz Henrique Brito Rios <lhbrios@tjba.jus.br>
2018-11-27 14:20:21 -0300

    loggedIn

--- src/layouts/Dashboard/Dashboard.jsx
+++ src/layouts/Dashboard/Dashboard.jsx
@@ -19,7 +19,7 @@ import dashboardStyle from "assets/jss/material-dashboard-react/layouts/dashboar
 import image from "assets/img/sidebar-2.jpg";
 import logo from "assets/img/reactlogo.png";
 
-var loggedIn = false;
+var loggedIn = true;
 
 const switchRoutes = (
   <Switch>
